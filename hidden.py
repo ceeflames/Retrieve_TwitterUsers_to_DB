ad1ebe7526ebcf239c3ba9b0ef54ebc1760f418b
Divine Oworoma <ceeflames@yahoo.com>
2022-11-24 17:23:49 +0100

    Working with twitter api

---
+++ hidden.py
@@ -0,0 +1,11 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "9hazU99wj4EZBYcDG",
+            "consumer_secret": "OfWztKnECawRpXNOnTimiVZbK4jqrlPSvHxvl5Q2EHYJeEiK8x",
+            "bearer token": "AAAAAAAAAAAA5XFxRrKogKxb",
+            "token_key": "363814VZ2JcBwBv",
+            "token_secret": "z9jaoIVlEbAa3O6XjQJ2s9MA"}
